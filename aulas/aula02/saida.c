533d824e133d40d689c6764673fe89c9225a80e5
Filiperfranca <dev.filipefranca@gmail.com>
2026-03-11 10:58:01 -0300

    codigos da aula 2

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // inclusão de biblitoca da linguagem
+
+int main(void)
+{
+    // entrada
+
+    // processamento
+
+    // saída
+    printf("um texto qualquer\n");
+    printf("%i\n", 10 + 10);
+    printf("%f", 1.784);
+
+    return 0;
+}
