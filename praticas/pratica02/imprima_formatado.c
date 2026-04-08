662a3472db65e12355a7034d435a8481d84574c0
Filiperfranca <dev.filipefranca@gmail.com>
2026-04-08 08:51:49 -0300

    feat: conclui pratica02. Fecha #3

--- praticas/pratica02/imprima_formatado.c
+++ praticas/pratica02/imprima_formatado.c
@@ -14,17 +14,17 @@ int main(void)
     valor[0] = 39.99;
     valor[1] = 89.90;
     valor[2] = 19.99;
-    valor[3] = valor[0] + valor[1] + valor[2];
+    valor[3] = 229.85;
 
     printf("==============================\n");
     printf("      N O T A    L E G A L\n");
     printf("==============================\n");
     printf("Produto         Qtd Valor Unit\n");
-    printf("%s        %.3i      %.2f\n", produtos[0], quantidade[0], valor[0]);
-    printf("%s           %.3i      %.2f\n", produtos[1], quantidade[1], valor[1]);
-    printf("%s     %.3i      %.2f\n", produtos[2], quantidade[2], valor[2]);
+    printf("%-15s %03i %10.2f\n", produtos[0], quantidade[0], valor[0]);
+    printf("%-15s %03i %10.2f\n", produtos[1], quantidade[1], valor[1]);
+    printf("%-15s %03i %10.2f\n", produtos[2], quantidade[2], valor[2]);
     printf("==============================\n");
-    printf("Total                   %.2f\n", valor[3]);
+    printf("Total                   %5.2f\n", valor[3]);
 
     return 0;
 }
