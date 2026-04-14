c4755b3777a0ba7ac79e0de4c43711371209c13d
monalisamoldurasml <monalisamoldurasml@gmail.com>
2026-04-14 16:30:48 -0300

    feat: conclui pratica03. Fecha #5

---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define PI 3.14159265
+
+int main(void)
+{
+
+    const double euler = 2.71828182;
+
+    printf("%.8f\n", PI);
+    printf("%.8f\n", euler);
+
+    return 0;
+}
